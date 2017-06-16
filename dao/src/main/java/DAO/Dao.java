9d523695ba8d430a19939e58939a7e15f8b406de
mandel <mandel.vaubourg@viacesi.fr>
2017-06-16 15:05:33 +0200

    DAO setTest
    Test is Valide for DaoTest

--- dao/src/main/java/DAO/Dao.java
+++ dao/src/main/java/DAO/Dao.java
@@ -13,7 +13,7 @@ public class Dao {
     /**
      * connection url
      * */
-    private final static String url = "jdbc:mysql://localhost:32769/boulderdash";
+    private final static String url = "jdbc:mysql://localhost:32769/boulderdash?autoReconnect=true&useSSL=false";
     /**
      * username
      * */
@@ -26,7 +26,7 @@ public class Dao {
      * Connection attribut
      * */
     private static Connection connection = null;
-    private static Dao dao;
+    private static Dao dao = null;
 
 
     /**
@@ -44,7 +44,7 @@ public class Dao {
      * Allow to access DAO instance to take connection.
      * @return Dao instance
      * */
-    public Dao getInstance(){
+    public static Dao getInstance(){
         if(dao == null){
             synchronized(Dao.class){
                 if(dao == null)
