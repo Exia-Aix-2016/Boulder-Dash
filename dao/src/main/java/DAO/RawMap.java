522843f8685eed6449301bff8f33257bb498dd03
MandelV <mandel.vaubourg@viacesi.fr>
2017-06-18 00:45:24 +0200

    add/edit JavaDoc add comment info

--- dao/src/main/java/DAO/RawMap.java
+++ dao/src/main/java/DAO/RawMap.java
@@ -23,16 +23,26 @@ public class RawMap {
      * */
     private ArrayList<RawElement> elements;
 
+    /**
+     * RawMap constructor
+     * @param nameMap Name of this Map
+     * @param dimension Dimension of this RawMap
+     * */
     public RawMap(final String nameMap, final Dimension dimension){
         this.name = nameMap;
         this.dimension = dimension;
         elements = new ArrayList<>();
-    }
+    }//FINISH
 
     //Mutateur
+    /**
+     * Add Element in Map Array
+     * @param element RawElement
+     * @see RawElement
+     * */
     public void addElement(final RawElement element){
         this.elements.add(element);
-    }
+    }//FINISH
 
     //Assesseur
     /**
@@ -40,21 +50,21 @@ public class RawMap {
      * */
     public String getName() {
         return name;
-    }
+    }//FINISH
     /**
      * @return Dimension of this Map
      * @see Dimension
      * */
     public Dimension getDimension() {
         return dimension;
-    }
+    }//FINISH
     /**
      * @return RawElement Array of this Map
      * @see RawElement
      * */
     public ArrayList<RawElement> getElements() {
         return elements;
-    }
+    }//FINISH
 
 
 
