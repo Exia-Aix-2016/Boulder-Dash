619e08b8f0ae43f53469858f17a92be1a58ba5be
MandelV <mandel.vaubourg@viacesi.fr>
2017-06-17 15:28:06 +0200

    sql add Procedure : addMapElement()

--- DataBase/createProcedure.sql
+++ DataBase/createProcedure.sql
@@ -18,7 +18,6 @@ BEGIN
 END |
 DELIMITER ;
 
-
 DELIMITER |
 CREATE PROCEDURE boulderdash.addMap(IN nameMap VARCHAR(255), IN width int, IN heigh int)
 BEGIN
@@ -31,4 +30,11 @@ CREATE PROCEDURE boulderdash.removeMap(IN nameMap VARCHAR(255))
 BEGIN
   DELETE FROM boulderdash.Map WHERE Nom = nameMap;
 END |
+DELIMITER ;
+
+DELIMITER |
+CREATE PROCEDURE boulderdash.addMapElement(IN mapId int, IN nameElement VARCHAR(255), IN X int, IN Y int)
+  BEGIN
+    INSERT INTO boulderdash.ObjectMap (CoordX, CoordY, IdMap, Type) VALUES (X, Y, mapId, nameElement);
+  END |
 DELIMITER ;
