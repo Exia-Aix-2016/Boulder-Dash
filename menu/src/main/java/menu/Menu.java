9113afed59c768cb68c618388bd8a32ea1420e9b
Hougo13 <hougo13@gmail.com>
2017-06-19 16:28:51 +0200

    add menu

---
+++ menu/src/main/java/menu/Menu.java
@@ -0,0 +1,55 @@
+package view.menu;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
+public class Menu extends JPanel implements IMenuAction{
+
+    private JPanel rootPanel;
+    private JPanel worldsPanel;
+
+    private Image backgroundImage;
+
+    public Menu(){
+        this.setLayout(new GridBagLayout());
+
+        try {
+            this.backgroundImage = ImageIO.read(this.getClass().getResource("background.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        this.rootPanel = new RootPanel(this);
+        this.worldsPanel = new WorldsPanel(this);
+
+        this.setPanel(this.rootPanel);
+    }
+
+    private void setPanel(JPanel panel){
+        this.removeAll();
+        this.add(panel);
+        this.revalidate();
+        this.repaint();
+    }
+
+    public void paintComponent(Graphics g) {
+        g.drawImage(this.backgroundImage, 0, 0, this.getWidth(), this.getHeight(), null);
+    }
+
+    @Override
+    public void exit() {
+        System.exit(0);
+    }
+
+    @Override
+    public void home() {
+        this.setPanel(rootPanel);
+    }
+
+    @Override
+    public void enterPlay() {
+        this.setPanel(worldsPanel);
+    }
+}
---
+++ menu/src/main/java/menu/WorldsPanel.java
@@ -0,0 +1,16 @@
+package view.menu;
+
+class WorldsPanel extends SubMenu {
+
+    WorldsPanel(IMenuAction menu) {
+        super(3, menu);
+
+        MButton backButton = new MButton("Back");
+
+        this.add(new MButton("World 1"));
+        this.add(new MButton("World 2"));
+        this.add(backButton);
+
+        backButton.addActionListener((e) -> { this.menu.home(); });
+    }
+}
