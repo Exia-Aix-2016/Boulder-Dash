3420702dd81685b24063936dcf74d7e513e091c2
Hougo13 <hougo13@gmail.com>
2017-06-20 12:11:41 +0200

    change engine

--- engine/src/main/java/engine/Engine.java
+++ engine/src/main/java/engine/Engine.java
@@ -1,16 +1,32 @@
 package engine;
 
-import dao.IMap;
 import world.IWorld;
 
 import javax.swing.*;
 import java.util.Collection;
 
 public class Engine extends JPanel{
+
+    IWorld world;
+
+    public Engine(){
+        this.setLayout(null);
+    }
+
     public void loadWorld(IWorld world){
+        this.world = world;
+
+        this.loadComponents();
+
+        this.revalidate();
+        this.repaint();
+    }
+
+    private void loadComponents(){
         Collection<JComponent> components = world.getComponents();
 
         for (JComponent component: components){
+            System.out.println(component);
             this.add(component);
         }
     }
