0e622e97e9fdcb7db02ab377093c0259d85d9caa
NooGat <kinghyde111@gmail.com>
2017-06-20 09:40:08 +0200

    Add files to create and fill World1

---
+++ DataBase/fillWorld1.sql
@@ -0,0 +1,4 @@
+INSERT INTO boulderdash.ObjectMap (MapName, TypeObject, CoordX, CoordY) VALUES ('world1', 'CHARACTER', 1, 2);
+INSERT INTO boulderdash.ObjectMap (MapName, TypeObject, CoordX, CoordY) VALUES ('world1', 'DIAMOND', 2, 2);
+INSERT INTO boulderdash.ObjectMap (MapName, TypeObject, CoordX, CoordY) VALUES ('world1', 'DIAMOND', 3, 2);
+INSERT INTO boulderdash.ObjectMap (MapName, TypeObject, CoordX, CoordY) VALUES ('world1', 'ROCK', 1, 1);
