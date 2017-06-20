1ca6b1b71aef0fad42c0166a76dd5805f424c78b
MandelV <mandel.vaubourg@viacesi.fr>
2017-06-20 15:25:36 +0200

    add Context

---
+++ engine/src/main/java/engine/Context.java
@@ -0,0 +1,58 @@
+package engine;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Collection;
+import java.util.Optional;
+
+/**
+ *Allow to check if the action of a entity is possible, this Thread return the block found
+ * @see Runnable
+ * @see JComponent
+ */
+public class Context implements Runnable  {
+
+    private Rectangle rectangle;
+    private Collection<JComponent> worldComponents;
+    private JComponent foundCompenent = null;
+
+
+    /**
+     * Contructor Context
+     * @param worldComponents Collection(JComponent)
+     * @param rectangle the cursor element to search in worldComponents
+     * */
+    public Context(Collection<JComponent> worldComponents, Rectangle rectangle){
+        this.rectangle = rectangle;
+        this.worldComponents = worldComponents;
+    }
+
+    /**
+     * Allow to calculate the context (check collision)
+     * */
+    @Override
+    public void run() {
+        for(JComponent component : worldComponents){
+            if(rectangle.intersects(component.getBounds())){
+                foundCompenent = component;
+            }
+        }
+    }
+
+    /**
+     * Allow to get the context this method start the thread context
+     * @return Optional(JComponent) if the context found the block return JComponent else Optional.empty()
+     * @see JComponent
+     * @see Optional
+     * @see Runnable
+     * */
+    public Optional<JComponent> getContext(){
+        Thread startCalcContext = new Thread(this);
+        startCalcContext.start();
+        if(foundCompenent != null){
+            return Optional.of(foundCompenent);
+        }else {
+            return Optional.empty();
+        }
+    }
+}
