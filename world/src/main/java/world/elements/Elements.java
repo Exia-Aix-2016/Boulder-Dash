348430c4bacf42b50eee8c9cb22fa1d588103e25
MandelV <mandel.vaubourg@viacesi.fr>
2017-06-21 18:03:36 +0200

    remove unused context

--- world/src/main/java/world/elements/Elements.java
+++ world/src/main/java/world/elements/Elements.java
@@ -10,7 +10,6 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
