566d5235dc5f7bfd7cc43e1db8c94fd24010a14c
Hougo13 <hougo13@gmail.com>
2017-06-23 12:24:45 +0200

    java doc remove

--- world/src/main/java/world/reaction/Remove.java
+++ world/src/main/java/world/reaction/Remove.java
@@ -4,31 +4,44 @@ import world.elements.IAction;
 import world.elements.entity.IMovement;
 
 /**
- * class Remove
- *
- * TODO
+ * The type Remove is a reaction which define that an object can be removed by an other.
  *
  * @see Reaction
  * @see IMovement
  * @see IAction
  * */
-
 public class Remove extends Reaction<IMovement, IAction> {
 
     private int delay;
+
     /**
-     * TODO
-     * */
+     * Instantiates a new Remove.
+     *
+     * @param to        the to
+     * @param typeClass the type class
+     * @param sides     the sides
+     * @param delay     the delay
+     */
     public Remove(IAction to, Class<? extends IMovement> typeClass, Sides[] sides, int delay){
         super(to, typeClass, sides);
         this.delay = delay;
     }
 
+    /**
+     * Instantiates a new Remove.
+     *
+     * @param to        the to
+     * @param typeClass the type class
+     * @param sides     the sides
+     */
     public Remove(IAction to, Class<? extends IMovement> typeClass, Sides[] sides){
         super(to, typeClass, sides);
         this.delay = 0;
     }
 
+    /**
+     * Remove the to object when a delay is reach and slow the from object
+     */
     @Override
     protected boolean execute(int ticks) {
         if (ticks > this.delay){
