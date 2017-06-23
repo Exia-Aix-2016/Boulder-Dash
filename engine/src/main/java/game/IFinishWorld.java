ef3b3cad9288ea6cbcf7e7bc16bd9c7bd06c8fb7
MandelV <mandel.vaubourg@viacesi.fr>
2017-06-23 12:29:44 +0200

    edit javadoc

--- engine/src/main/java/game/IFinishWorld.java
+++ engine/src/main/java/game/IFinishWorld.java
@@ -4,5 +4,8 @@ package game;
  * Represente the methods which call when the game is finish.
  */
 public interface IFinishWorld {
+    /**
+     * Call when the game is finish
+     * */
     void finished(boolean win);
 }
